c4f6fe98f1df5d4ffeaae4ab712f7f68403c2c1a
JOSE HENRIQUE <carvalhocostajr@gmail.com>
2025-03-28 16:41:29 +0000

    Primeira CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio das cartas!\n");
+    printf("desafio das cartas\n");
 
-    return 0;
+    char estado = 'A';
+    char código[10] = "A01";
+    char cidade[10] = "Belém";
+    int população = 1303403;
+    float área = 1059.47;
+    float PIB = 21191.47;
+    int Pturísticos = 23;
 
-}
-
-
+    printf("Carta 1\n");
+    printf("Estado: %c\n", estado);
+    printf("Códico: %s\n", código);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d de Habitantes\n", população);
+    printf("Área: %.2f km²\n", área);
+    printf("PIB: %.2f de Reais\n");
+    printf("Pontos Turísticos: %d\n");
     
 
+
+
+
+
+}
